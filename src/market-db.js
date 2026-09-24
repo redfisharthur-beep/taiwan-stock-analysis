09061f4b999d3e1ac1413db4935b1b8da5c72ed9
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-24 17:01:52 +0800

    Repair market page SQL search escaping and query parameters

--- src/market-db.js
+++ src/market-db.js
@@ -67,18 +67,8 @@ export async function getMarketPage(db,{market="all",query="",page=1,pageSize=30
  const search=term?" AND (c.stock LIKE ? ESCAPE '\\' OR c.name LIKE ? ESCAPE '\\')":"";
  const clause=` WHERE c.last_scan_at=(SELECT MAX(last_scan_at) FROM companies)${filter}${search}`;
  const args=(["上市","上櫃"].includes(market)?[market]:[]).concat(term?[
-  "%"+term.replace(/[\\%_]/g,"\\ const search=term?" AND (c.stock LIKE ? ESCAPE '\\\\' OR c.name LIKE ? ESCAPE '\\\\')":"";
- const clause=` WHERE c.last_scan_at=(SELECT MAX(last_scan_at) FROM companies)${filter}${search}`;
- const args=(["上市","上櫃"].includes(market)?[market]:[]).concat(term?[
-  "%"+term.replace(/[\\\\%_]/g,"\\\\$&")+"%",
-  "%"+term.replace(/[\\\\%_]/g,"\\\\$&")+"%"]:[]);
-")+"%",
-  "%"+term.replace(/[\\%_]/g,"\\ const search=term?" AND (c.stock LIKE ? ESCAPE '\\\\' OR c.name LIKE ? ESCAPE '\\\\')":"";
- const clause=` WHERE c.last_scan_at=(SELECT MAX(last_scan_at) FROM companies)${filter}${search}`;
- const args=(["上市","上櫃"].includes(market)?[market]:[]).concat(term?[
-  "%"+term.replace(/[\\\\%_]/g,"\\\\$&")+"%",
-  "%"+term.replace(/[\\\\%_]/g,"\\\\$&")+"%"]:[]);
-")+"%"]:[]);
+  "%"+term.replace(/[\\%_]/g,"\\$&")+"%",
+  "%"+term.replace(/[\\%_]/g,"\\$&")+"%"]:[]);
  const size=Math.max(1,Math.min(50,Math.trunc(Number(pageSize))||30));
  const p=Math.max(1,Math.min(10000,Math.trunc(Number(page))||1));
  const total=(await db.prepare("SELECT COUNT(*) AS n FROM companies c"+clause).bind(...args).first())?.n||0;
